a23f2c8c3f8d923c090f5f41c060068136986bda
Clément SIPIETER <c.sipieter@gmail.com>
2011-09-16 16:23:43 +0200

    Job #26 - Ajout des informations concernant la licence

--- Memento.java
+++ Memento.java
@@ -1,3 +1,23 @@
+/*  
+ *  Copyright © 2008-2011 Sipieter Clément <c.sipieter@gmail.com>
+ *  Copyright © 2011 Sellem Lev-Arcady
+ *
+ *  This file is part of Memento.
+ *
+ *  Memento is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Memento is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Memento.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
